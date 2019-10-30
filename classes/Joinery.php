428e5b38de5d8febf0aff000565e5e17da79cb43
Dawid Wojdyła <dawidw23@poczta.onet.pl>
2019-10-30 21:39:49 +0100

    Logowanie stanowiskami - Joinery -> funkcja login()

--- classes/Joinery.php
+++ classes/Joinery.php
@@ -1,16 +1,16 @@
 <?php
 class Joinery extends MyDB
 {
-	public $loggedUser = null;
+	public $stand = null;
 	
 	function __construct($host, $user, $password, $dbName, $dbType= 'mysql', $charset ='utf8'){
 		$this->dbo = $this->initDB($host, $user, $password, $dbName, $dbType= 'mysql', $charset ='utf8');
-		$this->loggedUser = $this->getActualUser();
+		$this->stand = $this->getActualStand();
 	}
 	
-	function getActualUser(){
-		if(isset($_SESSION['loggedUser'])){
-			return $_SESSION['loggedUser'];
+	function getActualStand(){
+		if(isset($_SESSION['stand'])){
+			return $_SESSION['stand'];
 		}
 		else{ 
 			return null;
@@ -31,49 +31,48 @@ class Joinery extends MyDB
 			return null;
 		}
 	}
-
+	
 	function login(){
 		if( !$this->dbo) return SERVER_ERROR;
-		if ($this->loggedUser) return NO_LOGIN_REQUIRED;
+		if ($this->stand) return NO_LOGIN_REQUIRED;
 		 
-		if (!isset($_POST["username"]) || !isset($_POST["password"])) return FORM_DATA_MISSING;
+		if (!isset($_POST["standId"]) || !isset($_POST["password"])) return FORM_DATA_MISSING;
 		
-		$username = filter_input(INPUT_POST, 'username');
+		$standId = filter_input(INPUT_POST, 'standId');
 		$password = filter_input(INPUT_POST, 'password');
 		
-		$usernameLength = mb_strlen($username, 'utf8');
 		$passwordLength = mb_strlen($password, 'utf8');
 		
-		if($usernameLength < 2 || $usernameLength >20 || $passwordLength < 4 || $passwordLength > 15){
+		if($standId < 1 || $standId > 4 || $passwordLength < 2 || $passwordLength > 10){
 			return ACTION_FAILED;
 		}
 		
-		$query = "SELECT `id`, `password` FROM `users` WHERE `username`=:username";
+		$query = "SELECT `name`, `password` FROM `stands` WHERE `id`=:standId";
 		$PDOstatement = $this->dbo->prepare($query);
-		$PDOstatement -> bindValue(':username', $username, PDO::PARAM_STR);
+		$PDOstatement -> bindValue(':standId', $standId, PDO::PARAM_INT);
 
-		if(!$PDOstatement->execute())
+		if(!$PDOstatement->execute()){
 			return SERVER_ERROR;
-		
-		if(!$result = $PDOstatement->fetch(PDO::FETCH_NUM))
-		  return ACTION_FAILED;
-		
-		 if(!password_verify($password, $result[1]))
-			return ACTION_FAILED;
-		 
-		$_SESSION['loggedUser'] = new User($result[0], $username);
-		if(!$result = $this->dbo->query("SELECT `permission_id` FROM `users_permissions` WHERE user_id={$_SESSION['loggedUser']->id}")){
-			return NO_PERMISSIONS;
 		}
-		$_SESSION['loggedUser']->permissions = $result->fetchAll();
+		
+		if(!$result = $PDOstatement->fetch(PDO::FETCH_NUM)){
+		  return ACTION_FAILED;
+		}
+		
+		 if(!password_verify($password, $result[1])){
+			return ACTION_FAILED;
+		 }
+		 
+		$_SESSION['stand'] = new Stand($standId, $result[0]);
+		
 		return ACTION_OK;
 	}
 	  
 	function logout()
 	{
-		$this->loggedUser = null;
-		if (isset($_SESSION['loggedUser']))
-		unset($_SESSION['loggedUser']);
+		$this->stand = null;
+		if (isset($_SESSION['stand']))
+		unset($_SESSION['stand']);
 	}
   
 }
